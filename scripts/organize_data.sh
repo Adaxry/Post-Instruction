7ab734bb1831ecfb03abcd3cca34d50e1f0e17ac
Adaxry <lyj14282008@gmail.com>
2023-08-24 21:01:06 +0800

    Update organize_data.sh

--- scripts/organize_data.sh
+++ scripts/organize_data.sh
@@ -1,2 +1,27 @@
 # for machine translation
+python convert_pair_to_alpaca.py \
+	-sf $your_source_file \
+	-tf $your_target_file \
+	-s  $source_language -t $target_language \
+	-if instruct_follow.filtered.trg_zh.post_ins.txt \
+	-of $output_name.json  \
+	--fix-cxt 3
+python convert_alpaca_to_hf.py \
+	-i $output_name.json \
+	-o $output_name.hf.json \
+	--above-prompt  # for post-ins
 
+
+# for text summarization
+python convert_pair_to_alpaca.py \
+	-sf $your_source_file \
+	-tf $your_target_file \
+	-s  en -t en \
+	-if instruct_follow_summary_post_ins.txt \
+	-of $output_name.json  \
+	--fix-cxt 3
+
+python convert_alpaca_to_hf.py \
+	-i $output_name.json \
+	-o $output_name.hf.json \
+	--above-prompt  # for post-ins
